8dc5c28e736f8b476b0df3e58598db8bf4f65c8a
Dominik Berner <dominik.berner@bbv.ch>
2018-07-13 09:26:10 +0200

    consistency check works

--- check_consistency.cc
+++ check_consistency.cc
@@ -11,6 +11,9 @@ struct A
 // could use std::enable if to create a type failure if Ts are not instances of
 // A
 
+template <typename T>
+constexpr bool check(T a) { return true; }
+
 template <typename... Ts>
 constexpr bool check(Ts... list_of_a)
 {
@@ -31,11 +34,13 @@ constexpr bool check(T t, T1 t1, Ts... list_of_a)
   // static_assert(T::id::value != T1::id::value, "No duplicates");
   // check<T1, Ts...>(t1, list_of_a...);
   static_assert(T::id::value != T1::id::value, "No duplicates");
-  return check(t, list_of_a...);
+  return check(t, list_of_a...) && check(t1, list_of_a...);
 }
 
 int main(int, char **)
 {
-  check(A<1>(), A<2>(), A<3>());
-  check(A<1>(), A<2>(), A<2>());
+  check(A<1>(), A<2>(), A<3>()); // OK
+  // check(A<1>(), A<1>(), A<2>()); // static assert triggered
+  // check(A<1>(), A<2>(), A<2>()); // static assert triggered
+  // check(A<1>(), A<2>(), A<1>()); // static assert triggered
 }
